e70b2333eb0ccac6a9b13667d0958583b6488787
Harshitank <harshitaaank@gmail.com>
2026-02-24 16:11:52 +0530

    Updated AVR code

--- avr-gcc/codes/main.c
+++ avr-gcc/codes/main.c
@@ -4,17 +4,14 @@
 
 int main(void)
 {
-    // Set PB0–PB3 (Arduino D8–D11) as OUTPUT
     DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB2) | (1<<PB3);
 
     while(1)
     {
         for(uint8_t i = 0; i < 10; i++)
         {
-            // Clear lower 4 bits
             PORTB &= 0xF0;
 
-            // Send BCD number to 7447
             PORTB |= (i & 0x0F);
 
             _delay_ms(1000);
