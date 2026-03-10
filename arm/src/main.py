d689495e2cf0720935a0ab1b7eea2bd7c956a846
Harshitank <harshitaaank@gmail.com>
2026-03-10 11:03:14 +0530

    Replaced arm.py with main.py

--- arm/src/main.py
+++ arm/src/main.py
@@ -1,19 +1,14 @@
-from machine import Pin
+import numpy as np
 import time
 
-led = Pin(15, Pin.OUT)
+led = 0
 
-a = Pin(4, Pin.OUT)
-b = Pin(5, Pin.OUT)
-c = Pin(6, Pin.OUT)
-d = Pin(7, Pin.OUT)
-e = Pin(8, Pin.OUT)
-f = Pin(9, Pin.OUT)
-g = Pin(10, Pin.OUT)
+a=b=c=d=e=f=g=0
 
-minterms = [0,1,3,5,9,10,14]
+minterms = np.array([0,1,3,5,9,10,14])
 
 def display_digit(num):
+
     digits = {
         0:(0,0,0,0,0,0,1),
         1:(1,0,0,1,1,1,1),
@@ -26,31 +21,28 @@ def display_digit(num):
         8:(0,0,0,0,0,0,0),
         9:(0,0,0,0,1,0,0)
     }
-    
+
     if num in digits:
         val = digits[num]
-        a.value(val[0])
-        b.value(val[1])
-        c.value(val[2])
-        d.value(val[3])
-        e.value(val[4])
-        f.value(val[5])
-        g.value(val[6])
+        print("Segments:",val)
 
 while True:
+
     for num in range(16):
-        
-        
+
         display_digit(num % 10)
-        led.value(0)
+        led = 0
+        print("LED:",led)
+
         time.sleep(1)
 
-        
         if num in minterms:
             display_digit(1)
-            led.value(1)
+            led = 1
         else:
             display_digit(0)
-            led.value(0)
-            
+            led = 0
+
+        print("LED:",led)
+
         time.sleep(1)
